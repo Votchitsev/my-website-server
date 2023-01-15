4d03d59007375d4d64b75c9ca8ccecd58778f39c
Dmitry <dmitry.votchitsev@gmail.com>
2023-01-15 09:01:24 +0200

    router get edu

--- router/education_company/education_company_crud.py
+++ router/education_company/education_company_crud.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from database.database import SessionLocal
 from database.schemas import EducationCompany
 from router.education_company.create import create
+from router.education_company.get import get
 
 
 router = APIRouter(
@@ -13,3 +14,8 @@ router = APIRouter(
 @router.post('/')
 def create_edu(data: EducationCompany):
     create(SessionLocal(), data)
+
+
+@router.get('/')
+def get_edu(data: EducationCompany, lang: str):
+    get(SessionLocal(), lang)
