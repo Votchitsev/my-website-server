d15fd207cdc5221518557f78b12a65dd8a4ac924
Dmitry <dmitry.votchitsev@gmail.com>
2023-01-15 12:25:05 +0200

    skills crud

--- router/skills/skills_crud.py
+++ router/skills/skills_crud.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from database.database import SessionLocal
 from database.schemas import Skill
 from router.skills.create import create
+from router.skills.get import get
 
 
 router = APIRouter(
@@ -13,3 +14,9 @@ router = APIRouter(
 @router.post('/')
 def create_skill(data: Skill):
     create(SessionLocal(), data)
+
+
+@router.get('/')
+def get_skill(lang: str, edu_id: int):
+    return get(SessionLocal(), lang, edu_id)
+    
