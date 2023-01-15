6962266c36a5a8218a8d1dbd36acd43b47094281
Dmitry <dmitry.votchitsev@gmail.com>
2023-01-15 08:58:26 +0200

    get edu function

---
+++ router/education_company/get.py
@@ -0,0 +1,19 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+from database.models import EducationCompany, Language
+
+
+def get(db: Session, lang):
+    query_response = db.query(EducationCompany).join(Language).filter(Language.name == lang).first()
+
+    if query_response:
+        return {
+            "name": query_response.name,
+            "start_education_date": query_response.start_education_date,
+            "finish_education_date": query_response.finish_education_date,
+            "logo": query_response.logo,
+        }
+    
+    raise HTTPException(status_code=404, detail="Education conpany not found")
+    
